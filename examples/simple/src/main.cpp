206773249807075ac828918b956262fa3be8cf72
s-martin <saimen54@hotmail.com>
2018-11-24 15:57:32 +0100

    added simple example

---
+++ examples/simple/src/main.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <ostream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Simple cmake-tools example project successful" << std::endl;
+    return 0;
+}
